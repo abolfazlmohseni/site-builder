0a0a573c3cc5b9049e6ad4afeba23cd193ef96d4
Abolfazl Mohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2024-12-13 20:29:06 +0330

    Theme and Landing Page

--- asset/home/js/script.js
+++ asset/home/js/script.js
@@ -1,8 +1,31 @@
-const LoaderElem = document.querySelector(".Loader")
-
-function LoadedDocumentHandler() {
-    LoaderElem.style.display = "none"
+let $ = document
+const themeBTN = $.querySelector(".theme")
+const bodyElem = $.querySelector("body")
+let flagTheme = localStorage.getItem("theme")
+// theme Website
+if (flagTheme == "dark") {
+    bodyElem.classList = "darkB"
 }
 
+themeBTN.addEventListener("click", function () {
+    if (bodyElem.classList == "darkB") {
+        bodyElem.classList = ""
+        localStorage.setItem("theme", "Light")
+    }
+    else {
+        bodyElem.classList = "darkB"
+        localStorage.setItem("theme", "dark")
+    }
+})
+// theme Website End !
 
-document.addEventListener("DOMContentLoaded",LoadedDocumentHandler)
+
+//Loader Website!
+// const LoaderElem = document.querySelector(".Loader")
+
+// function LoadedDocumentHandler() {
+//     LoaderElem.style.display = "none"
+// }
+
+
+// document.addEventListener("DOMContentLoaded",LoadedDocumentHandler)

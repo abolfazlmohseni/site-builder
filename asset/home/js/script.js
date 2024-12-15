fe15d4189fc84f76b94358eb225d44b9cc6babb6
Abolfazl Mohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2024-12-15 22:55:27 +0330

    commit

--- asset/home/js/script.js
+++ asset/home/js/script.js
@@ -18,7 +18,54 @@ themeBTN.addEventListener("click", function () {
     }
 })
 // theme Website End !
+// slider prodact
+let i = 0
+prodactElem = $.querySelectorAll(".sectionTree-layers__items")
+setInterval(function () {
+    if (i == 0) {
+        prodactElem[i].classList = "sectionTree-layers__items layers-active"
+        prodactElem[i + 1].classList = "sectionTree-layers__items layers-left"
+        prodactElem[i + 2].classList = "sectionTree-layers__items layers-right"
 
+    } else if (i < 9) {
+        prodactElem[i].classList = "sectionTree-layers__items layers-active"
+        prodactElem[i + 1].classList = "sectionTree-layers__items layers-left"
+        prodactElem[i + 2].classList = "sectionTree-layers__items layers-right"
+
+        prodactElem[i - 1].classList = "sectionTree-layers__items layers-active"
+        prodactElem[i - 2].classList = "sectionTree-layers__items layers-left"
+        prodactElem[i - 3].classList = "sectionTree-layers__items layers-right"
+        console.log("salam")
+    } else if (i < 10) {
+        prodactElem[i].classList = "sectionTree-layers__items layers-active"
+        prodactElem[i + 1].classList = "sectionTree-layers__items layers-left"
+        prodactElem[i - 1].classList = "sectionTree-layers__items layers-right"
+        console.log("salam")
+
+        prodactElem[i - 1].classList = "sectionTree-layers__items layers-active"
+        prodactElem[i - 2].classList = "sectionTree-layers__items layers-left"
+        prodactElem[i - 3].classList = "sectionTree-layers__items layers-right"
+    } else if (i == 10) {
+        prodactElem[i].classList = "sectionTree-layers__items layers-active"
+        prodactElem[i - 1].classList = "sectionTree-layers__items layers-left"
+        prodactElem[i - 2].classList = "sectionTree-layers__items layers-right"
+        console.log("salam")
+
+        prodactElem[i - 1].classList = "sectionTree-layers__items layers-active"
+        prodactElem[i - 3].classList = "sectionTree-layers__items layers-left"
+        prodactElem[i - 4].classList = "sectionTree-layers__items layers-right"
+    } else {
+        i = 0
+        console.log("reset time")
+    }
+    i++
+}, 2000)
+
+
+
+
+
+// slider prodact End !
 
 //Loader Website!
 // const LoaderElem = document.querySelector(".Loader")
